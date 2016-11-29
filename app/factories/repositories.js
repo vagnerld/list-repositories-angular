96c4d0afcb5efbd91c2a3a26794f97c3eb6f549a
Vagner Dutra <vagnerdutra@outlook.com>
2016-11-29 21:50:33 -0200

    :-1:

--- app/factories/repositories.js
+++ app/factories/repositories.js
@@ -9,7 +9,7 @@ app.factory('Repositories', function ($http) {
              * Retorna json com a lista de repositórios publicos de um usuário
              */
              getPublicRepositories: function ($user){
-                return $http('https://api.github.com/users/'+ $user +'/repos').get();
+                return $http.get('https://api.github.com/users/'+ $user +'/repos');
             }
         };
     }
