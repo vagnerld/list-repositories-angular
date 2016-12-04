6a98cb61eb8dad1129d3e6e4ce2b6fc60e0b4a1e
Vagner Dutra <vagnerdutra@outlook.com>
2016-12-04 12:02:03 -0200

    add boas práticas

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,26 +1,26 @@
-'use strict';
+(function() {
+	'use strict';
 
-/*
- * 
- */
-app.controller('homeController', ['$scope', 'Repositories', 
-	function($scope, Repositories) {
-	
-		$scope.user = 'vagnerld'; // user default
-		$scope.repositories = [];
+	/*
+	 * Home page Controller
+	 */
+	app.controller('homeController', ['$scope', 'Repositories', 
+		function($scope, Repositories) {
 
-		$scope.listRepositories = function() {
-		
-		    Repositories.getPublicRepositories( $scope.user ).then(function(repositorie){
-		        if ( repositorie.status == 200 ){
-		        	$scope.repositories = repositorie.data;
-		        }
-		    });
-		};
+			$scope.user = 'vagnerld'; // user default
+			$scope.repositories = []; // list of repositories
 
-		$scope.listRepositories();
-		
-		
-	    
-	}]
-);
+			$scope.listRepositories = function() {
+			    Repositories.getPublicRepositories( $scope.user ).then(function(repositorie){
+				if ( repositorie.status == 200 ){
+					$scope.repositories = repositorie.data;
+				}
+			    });
+			};
+
+			$scope.listRepositories();
+
+		}]
+	);
+
+})();
