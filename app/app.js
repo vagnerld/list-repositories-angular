0df56c676faefc4bceca950872e41182e5e64673
Vagner Dutra <vagnerdutra@outlook.com>
2016-12-04 11:52:54 -0200

    atualizando rotas

--- app/app.js
+++ app/app.js
@@ -1,17 +1,15 @@
 var app = angular.module('app', ['ngRoute']);
 
-// configure our routes
-app.config(function($routeProvider) {	
-    $routeProvider
-
-        // rotas da nossa SPA
-
-        // route home page
-        .when('/', {
-            templateUrl : 'app/views/home.html',
-            controller  : 'homeController'
-        })
-        .otherwise({
-		    redirectTo: "/"
-		});
+// routes
+app.config(function($routeProvider) {
+	$routeProvider
+	// route home page
+	.when('/', {
+	    templateUrl : 'app/views/home.html',
+	    controller  : 'homeController'
+	})
+	.otherwise({
+	    redirectTo: "/"
+	});
+	
 });
